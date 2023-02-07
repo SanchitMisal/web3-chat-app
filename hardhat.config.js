8f1cea7e4febc8192e102effb74ab12d7887b828
Shadab Akhtar <akhtarshadab415@gmail.com>
2023-02-07 23:38:25 +0530

    ⚙️ configure blockchain for contract deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const { MNEMONIC, APOTHEM_RPC_URL } = process.env;
+
+const DEFAULT_MNEMONIC =
+  "disorder pizza plate good dream delay cave broom width gaze glove fossil";
+
+const sharedNetworkConfig = {
+  accounts: {
+    mnemonic: MNEMONIC ?? DEFAULT_MNEMONIC,
+  },
+};
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -6,6 +18,10 @@ module.exports = {
   networks: {
     hardhat: {
       chainId: 1337,
+    },
+    apothem: {
+      ...sharedNetworkConfig,
+      url: APOTHEM_RPC_URL,
     },
   },
 };
