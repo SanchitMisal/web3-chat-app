f778e2d1b33c08ce07ff1671ead84e638a8f014a
Shadab Akhtar <akhtarshadab415@gmail.com>
2023-02-08 11:57:54 +0530

    ⚙️ contract configuration

--- Context/constants.js
+++ Context/constants.js
@@ -1,6 +1,6 @@
 //0x5FbDB2315678afecb367f032d93F642f64180aa3
 import chatApp from "./ChatApp.json";
 
-export const ChatAppAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const ChatAppAddress = "0x6ecCF1F424d3f0527780D28BF8ac1886500D3a83";
 
 export const ChatAppABI = chatApp.abi;
