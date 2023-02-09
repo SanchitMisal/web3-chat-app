f95a4e032320bc236f8597f773ad703256d8a5d4
SanchitMisal <72195684+SanchitMisal@users.noreply.github.com>
2023-02-09 18:58:13 +0530

    Bugs Fixed

--- Components/Model/Model.jsx
+++ Components/Model/Model.jsx
@@ -73,7 +73,7 @@ const Model = ({
                   {""}
                   <Image src={images.close} alt="send" width={30} height={30} />
                   {""}
-                  Not now
+                  Not Now
                 </button>
               </div>
             </div>
